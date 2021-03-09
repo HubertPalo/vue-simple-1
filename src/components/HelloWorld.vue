4861bce55f98506dbaa6d52bdc102fb08cdf76b1
Nil <darlinnep@gmail.com>
2021-03-09 08:43:04 -0300

    start

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,42 +1,88 @@
 <template>
-  <div class="hello">
-    <h1>{{ msg }}</h1>
-    <p>
-      For a guide and recipes on how to configure / customize this project,<br>
-      check out the
-      <a href="https://cli.vuejs.org" target="_blank" rel="noopener">vue-cli documentation</a>.
-    </p>
-    <h3>Installed CLI Plugins</h3>
-    <ul>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-babel" target="_blank" rel="noopener">babel</a></li>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-eslint" target="_blank" rel="noopener">eslint</a></li>
-    </ul>
-    <h3>Essential Links</h3>
-    <ul>
-      <li><a href="https://vuejs.org" target="_blank" rel="noopener">Core Docs</a></li>
-      <li><a href="https://forum.vuejs.org" target="_blank" rel="noopener">Forum</a></li>
-      <li><a href="https://chat.vuejs.org" target="_blank" rel="noopener">Community Chat</a></li>
-      <li><a href="https://twitter.com/vuejs" target="_blank" rel="noopener">Twitter</a></li>
-      <li><a href="https://news.vuejs.org" target="_blank" rel="noopener">News</a></li>
-    </ul>
-    <h3>Ecosystem</h3>
-    <ul>
-      <li><a href="https://router.vuejs.org" target="_blank" rel="noopener">vue-router</a></li>
-      <li><a href="https://vuex.vuejs.org" target="_blank" rel="noopener">vuex</a></li>
-      <li><a href="https://github.com/vuejs/vue-devtools#vue-devtools" target="_blank" rel="noopener">vue-devtools</a></li>
-      <li><a href="https://vue-loader.vuejs.org" target="_blank" rel="noopener">vue-loader</a></li>
-      <li><a href="https://github.com/vuejs/awesome-vue" target="_blank" rel="noopener">awesome-vue</a></li>
-    </ul>
+  <!--<ul id="example-1">
+    <li v-for="item in list" :key="item">
+      {{ item }}
+    </li>
+  </ul>-->
+  <div class="container">
+    <div class="block" v-for="item in list" :key="item.id">
+      <div
+        class="card has-background-link-light"
+        @click="
+          triggerModal();
+          selectItem(item);
+        "
+      >
+        <div class="card-content">
+          <div class="content">
+            <p>
+              <b>{{ item.attributes.titles.en_jp }}</b> [JP:{{
+                item.attributes.titles.ja_jp
+              }}]
+            </p>
+          </div>
+        </div>
+      </div>
+    </div>
+    <div
+      class="modal"
+      v-bind:class="{ 'is-active': showModal }"
+      v-if="selectedItem"
+    >
+      <div class="modal-background"></div>
+      <div class="modal-card">
+        <header class="modal-card-head">
+          <p class="modal-card-title">
+            <b>{{ selectedItem.attributes.titles.en_jp }}</b>
+          </p>
+        </header>
+        <section class="modal-card-body">
+          <p class="image is-4by3">
+            <img
+              :src="selectedItem.attributes.posterImage.medium"
+              alt=""
+            />
+          </p>
+          <p>{{ selectedItem.attributes.synopsis }}</p>
+        </section>
+      </div>
+      <button
+        class="modal-close is-large"
+        aria-label="close"
+        @click="triggerModal"
+      ></button>
+    </div>
   </div>
 </template>
 
 <script>
 export default {
-  name: 'HelloWorld',
+  name: "HelloWorld",
   props: {
-    msg: String
-  }
-}
+    msg: String,
+  },
+  data: function () {
+    return {
+      list: [],
+      showModal: false,
+      selectedItem: null,
+    };
+  },
+  methods: {
+    triggerModal() {
+      this.showModal = !this.showModal;
+    },
+    selectItem(newItem) {
+      this.selectedItem = newItem;
+    },
+  },
+  mounted() {
+    this.$http
+      .get("https://kitsu.io/api/edge/anime")
+      .then((response) => (this.list = response.data.data))
+      .catch((error) => console.log(error));
+  },
+};
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
